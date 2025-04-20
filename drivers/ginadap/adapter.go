b00bf41543a594c1536a4c9b475bf38a590f2abc
Wrtgvr <saldaniil123@gmail.com>
2025-04-20 20:18:27 +0700

    Folder renamed. Added: comments

---
+++ drivers/ginadap/adapter.go
@@ -0,0 +1,27 @@
+package ginadapter
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/wrtgvr2/errsuit"
+)
+
+// Send response via gin.Context with err HTTP status code, err message and err type (type e.g. `errsuit.TypeNotFound`).
+// If err is type of `error` then converts it to `AppError`.
+// Return `false` if err is nil, otherwise return true.
+func HandleError(c *gin.Context, err error) bool {
+	if err == nil {
+		return false
+	}
+	appErr := errsuit.AsAppError(err)
+
+	if appErr.ShouldLog() {
+		appErr.LogError()
+	}
+
+	c.JSON(appErr.Code, gin.H{
+		"error": appErr.Message,
+		"type":  appErr.Type,
+	})
+
+	return true
+}
