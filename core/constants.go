7786de526f399a18c2c954349a489ade170d7d1b
Wrtgvr <saldaniil123@gmail.com>
2025-05-18 17:52:45 +0700

    Added: response format constants

--- core/constants.go
+++ core/constants.go
@@ -9,3 +9,22 @@ const (
 	TypeForbidden    = "forbidden"
 	TypeConflict     = "conflict"
 )
+
+type ResponseFormat int
+
+// Response formats
+const (
+	ResponseFormatSmart ResponseFormat = iota
+	ResponseFormatJSON
+	ResponseFormatXML
+	ResponseFormatYAML
+	ResponseFormatPlainText
+)
+
+// Aliases for response formats
+var contentTypeAliases = map[ResponseFormat][]string{
+	ResponseFormatJSON:      {"application/json", "text/json", "+json"},
+	ResponseFormatXML:       {"application/xml", "text/xml", "+xml"},
+	ResponseFormatYAML:      {"application/x-yaml", "text/yaml", "application/yaml", "+yaml"},
+	ResponseFormatPlainText: {"text/plain"},
+}
