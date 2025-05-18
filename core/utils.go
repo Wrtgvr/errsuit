e6933bee54b17094ad7aa005a83583afd61a23ef
Wrtgvr <saldaniil123@gmail.com>
2025-05-18 17:54:06 +0700

    Added: func to determine response format, func to write error response

--- core/utils.go
+++ core/utils.go
@@ -1,5 +1,13 @@
 package errsuit
 
+import (
+	"encoding/json"
+	"encoding/xml"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
 func BuildErrorResp(appErr *AppError) ErrorResponse {
 	msg := appErr.Message
 	if appErr.Type == TypeInternal {
@@ -8,5 +16,51 @@ func BuildErrorResp(appErr *AppError) ErrorResponse {
 	return ErrorResponse{
 		msg,
 		appErr.Type,
+	}
+}
+
+func determineFormat(ctx Context, respFormat ResponseFormat) ResponseFormat {
+	if respFormat != ResponseFormatSmart {
+		return respFormat
+	}
+
+	accept := ctx.GetHeader("Accept")
+	accept = strings.ToLower(accept)
+
+	for format, aliases := range contentTypeAliases {
+		for _, alias := range aliases {
+			if strings.Contains(accept, alias) {
+				return format
+			}
+		}
+	}
+
+	// fallback
+	return ResponseFormatJSON
+}
+
+func WriteError(ctx Context, err *AppError, format ResponseFormat) {
+	actualFormat := determineFormat(ctx, format)
+	body := BuildErrorResp(err)
+
+	ctx.SetStatus(err.Code)
+
+	switch actualFormat {
+	case ResponseFormatJSON:
+		ctx.SetHeader("Content-Type", "application/json")
+		b, _ := json.Marshal(body)
+		ctx.Write(b)
+	case ResponseFormatYAML:
+		ctx.SetHeader("Content-Type", "application/x-yaml")
+		b, _ := yaml.Marshal(body)
+		ctx.Write(b)
+	case ResponseFormatXML:
+		ctx.SetHeader("Content-Type", "application/xml")
+		b, _ := xml.Marshal(body)
+		ctx.Write(b)
+	case ResponseFormatPlainText:
+		ctx.SetHeader("Content-Type", "text/plain")
+		b, _ := json.Marshal(body)
+		ctx.Write(b)
 	}
 }
